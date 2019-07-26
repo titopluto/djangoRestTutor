d149ed367a3a3d22a96a4d7c114d84264d004026
tito7 <b.tito@dal.ca>
2019-07-25 22:02:58 -0300

    added management command and db mock tests

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,19 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
+        }),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
